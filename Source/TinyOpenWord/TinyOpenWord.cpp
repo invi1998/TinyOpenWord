4dbe4555e36a7e8852a88c95badf1bb07dbd24c8
invi1998 <2267661826@qq.com>
2025-01-26 22:47:56 +0800

    初始化

---
+++ Source/TinyOpenWord/TinyOpenWord.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TinyOpenWord.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TinyOpenWord, "TinyOpenWord" );
