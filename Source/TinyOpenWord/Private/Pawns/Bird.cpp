26bbf09a905ab14774ea48145dfcdd74846e932a
invi1998 <2267661826@qq.com>
2025-01-27 22:57:31 +0800

    bird

---
+++ Source/TinyOpenWord/Private/Pawns/Bird.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright INVI1998, Inc. All Rights Reserved.
+
+
+#include "Pawns/Bird.h"
+
+
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/TinyOpenWord/Public/Pawns/Bird.h
@@ -0,0 +1,26 @@
+﻿// Copyright INVI1998, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class TINYOPENWORD_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+};
