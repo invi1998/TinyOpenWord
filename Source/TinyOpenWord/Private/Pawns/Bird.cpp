54c56dbd3883de3f44e33d4317ee2df32960b4bc
invi1998 <2267661826@qq.com>
2025-01-27 23:23:30 +0800

    设置鸟的胶囊体

--- Source/TinyOpenWord/Private/Pawns/Bird.cpp
+++ Source/TinyOpenWord/Private/Pawns/Bird.cpp
@@ -3,10 +3,18 @@
 
 #include "Pawns/Bird.h"
 
+#include "Components/CapsuleComponent.h"
+
 
 ABird::ABird()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	CapsuleComponent->InitCapsuleSize(34.0f, 88.0f);
+	CapsuleComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	CapsuleComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	RootComponent = CapsuleComponent;
 }
 
 void ABird::Tick(float DeltaTime)
--- Source/TinyOpenWord/Public/Pawns/Bird.h
+++ Source/TinyOpenWord/Public/Pawns/Bird.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Pawn.h"
 #include "Bird.generated.h"
 
+class UCapsuleComponent;
+
 UCLASS()
 class TINYOPENWORD_API ABird : public APawn
 {
@@ -22,5 +24,7 @@ protected:
 	virtual void BeginPlay() override;
 
 private:
+	UPROPERTY()
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
 
 };
