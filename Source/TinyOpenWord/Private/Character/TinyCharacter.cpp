bfe2a5eee775f3b9b9d928688467f979c0e4e683
invi1998 <2267661826@qq.com>
2025-01-31 13:16:21 +0800

    TinyCharacter 主角类

---
+++ Source/TinyOpenWord/Private/Character/TinyCharacter.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright INVI1998, Inc. All Rights Reserved.
+
+
+#include "Character/TinyCharacter.h"
+
+
+ATinyCharacter::ATinyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATinyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATinyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ATinyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/TinyOpenWord/Public/Character/TinyCharacter.h
@@ -0,0 +1,26 @@
+﻿// Copyright INVI1998, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TinyCharacter.generated.h"
+
+UCLASS()
+class TINYOPENWORD_API ATinyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ATinyCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+};
