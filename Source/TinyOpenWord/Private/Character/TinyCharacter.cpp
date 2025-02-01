e1e89d71698af5185432febf636905ede18f3399
invi1998 <2267661826@qq.com>
2025-02-01 13:34:49 +0800

    朝向移动方向

--- Source/TinyOpenWord/Private/Character/TinyCharacter.cpp
+++ Source/TinyOpenWord/Private/Character/TinyCharacter.cpp
@@ -4,12 +4,17 @@
 #include "Character/TinyCharacter.h"
 
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 
 ATinyCharacter::ATinyCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;	// 不使用控制器旋转俯仰
+	bUseControllerRotationYaw = false;		// 不使用控制器旋转偏航
+	bUseControllerRotationRoll = false;	// 不使用控制器旋转滚动
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
@@ -21,6 +26,9 @@ ATinyCharacter::ATinyCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom);
 	FollowCamera->bUsePawnControlRotation = false;	// 不使用控制器旋转
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;	// 朝向移动方向
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 360.0f, 0.0f);	// 旋转速度，只影响朝向移动方向
 }
 
 void ATinyCharacter::Tick(float DeltaTime)
